768c8063728b28bb2b4f3db6b990967e68e8ba32
fabezio <fabezio@outlook.fr>
2021-04-14 03:53:19 +0200

    selection des styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,13 +4,26 @@
 	let text = [];
 	let value = "";
 	let addText = "";
+	let fonts = [];
+	let style = "";
 	let font = "";
+	let fontButtons = [
+		{ font: "bold", label: "gras", isChecked: false },
+		{ font: "italic", label: "italique", isChecked: false },
+		{ font: "underlined", label: "souligné", isChecked: false },
+	];
+	function addFontStyle(fnt) {
+		fonts = [...fonts, fnt];
+		return "checked", fonts;
+	}
 	function newText() {
 		addText = value;
-		text = [...text, addText];
-		console.table(text);
+		fonts = [...fonts, font];
+		style = fonts.join(" ");
+		text = [...text, { content: addText, style }];
+		console.log(text);
 		value = "";
-		return text;
+		return text, style;
 	}
 	$: console.log(font);
 
@@ -25,7 +38,19 @@
 
 <form on:submit|preventDefault={newText}>
 	<textarea bind:value cols="100" placeholder="entrez votre texte ici" />
-	<!-- <select bind:value={}>
+	<!-- <select for="fonts">
+		<option name="fonts" type="checkbox" bind:value={font}>
+	</label> -->
+	<h3>Format du texte</h3>
+	{#each fontButtons as button}
+		<button
+			on:click={() => (button.isChecked = !button.isChecked)}
+			class="btn {button.isChecked
+				? addFontStyle(button.font)
+				: 'unchecked'}">{button.label}</button
+		>
+	{/each}
+	<!-- <select for="fonts">
 		<option name="normal">normal</option>
 		<option name="bold">gras</option>
 		<option name="underlined">souligné</option>
@@ -37,7 +62,7 @@
 <!-- <p>{addText}</p> -->
 <!-- <p>{font}</p> -->
 {#each text as entry}
-	<div>{entry}</div>
+	<div class={entry.style}>{entry.content}</div>
 {/each}
 <!-- 
 <figure>
@@ -73,6 +98,28 @@
 
 	p {
 		margin: 1em auto;
+	}
+	.btn {
+		border: none;
+		border-radius: 7px;
+		min-width: 10rem;
+		/* background: goldenrod; */
+	}
+	.btn:selected,
+	.checked {
+		background: yellowgreen;
+	}
+	.unchecked {
+		background: goldenrod;
+	}
+	.bold {
+		font-weight: bold;
+	}
+	.underlined {
+		text-decoration: underlined;
+	}
+	.italic {
+		font-style: italic;
 	}
 
 	@media (min-width: 480px) {
